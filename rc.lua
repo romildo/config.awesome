9e17783edfed73520b0afe21ed7a0ddec33d27ef
José Romildo Malaquias <malaquias@gmail.com>
2020-10-10 08:27:11 -0300

    Hotkeys for myawesomemenu

--- rc.lua
+++ rc.lua
@@ -184,11 +184,11 @@ altkey = "Mod1"
 -- {{{ Menu
 -- Create a launcher widget and a main menu
 myawesomemenu = {
-   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-   { "manual", terminal .. " -e man awesome" },
-   { "edit config", editor_cmd .. " " .. awesome.conffile },
-   { "restart", awesome.restart },
-   { "quit", function() awesome.quit() end },
+   { "&Hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
+   { "&Manual", terminal .. " -e man awesome" },
+   { "&Edit config", editor_cmd .. " " .. awesome.conffile },
+   { "Restart", awesome.restart },
+   { "Quit", function() awesome.quit() end },
 }
 
 mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
